afe2a89c20b9d9803be21d3410b706f63957417d
Rodion Roslovets <Rodion18570@gmail.com>
2019-12-09 21:12:40 +0300

    First commit

---
+++ build/precache-manifest.7cc976295a9a19ceb09add8c158c77a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7622af3a5d4621703c6f4d655286ac5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd505f2604f32df94df5",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "8aa2098e399a4e6aefce",
+    "url": "/static/js/2.2f670402.chunk.js"
+  },
+  {
+    "revision": "cd505f2604f32df94df5",
+    "url": "/static/js/main.79681f79.chunk.js"
+  },
+  {
+    "revision": "cc722314477955d37109",
+    "url": "/static/js/runtime-main.98cec8c9.js"
+  }
+]);
